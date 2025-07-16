19a3ef2f242a01fad0ef1de5b52c1ee34b46ce8d
Lucas Wilson <lucaswil291@gmail.com>
2025-07-16 22:30:15 +1000

    Add files via upload

---
+++ string.js
@@ -0,0 +1,9 @@
+let myString = "This is a string";
+let anotherString = "   Another string"; // 3 blank spaces at the beginning
+let hello = "Hello there!";
+let myName = "Lucas"; // Replace "Lucas" with your first name
+
+console.log("myString:", myString);
+console.log("anotherString:", anotherString);
+console.log("hello:", hello);
+console.log("myName:", myName);

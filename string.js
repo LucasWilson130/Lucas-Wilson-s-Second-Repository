784594d9aeec99248dc2387ef1119b42a00572e2
Lucas Wilson <lucaswil291@gmail.com>
2025-07-17 22:41:26 +1000

    Update string.js

--- string.js
+++ string.js
@@ -1,9 +1,17 @@
 let myString = "This is a string";
 let anotherString = "   Another string"; // 3 blank spaces at the beginning
 let hello = "Hello there!";
-let myName = "Lucas"; // Replace "Lucas" with your first name
+let myName = "Botanic"; // Replace "Botanic" with your first name
 
-console.log("myString:", myString);
-console.log("anotherString:", anotherString);
-console.log("hello:", hello);
-console.log("myName:", myName);
+// Find out the length of myString
+let myStringLength = myString.length;
+console.log("Length of myString:", myStringLength);
+
+// Find out the first character of myString
+let firstCharOfMyString = myString.charAt(0);
+console.log("First character of myString:", firstCharOfMyString);
+
+// Find out the 11th character of myString
+// Remember that string indices are 0-based, so the 11th character is at index 10.
+let eleventhCharOfMyString = myString.charAt(10);
+console.log("11th character of myString:", eleventhCharOfMyString);
